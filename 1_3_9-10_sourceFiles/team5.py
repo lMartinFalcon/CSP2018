ad725b31759928aff491e0db316af628c8ad736b
Paul Reed <811279@c02tn6h6gfwm.nkcschools.org>
2018-12-12 14:57:20 -0600

    Paul

--- 1_3_9-10_sourceFiles/team5.py
+++ 1_3_9-10_sourceFiles/team5.py
@@ -5,19 +5,37 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+from random import randint;
+team_name = 'BiLasagnash' # Only 10 chars displayed.
+strategy_name = 'All choices are bad choices.'
+strategy_description = '10 minutes later...'
     
 def move(my_history, their_history, my_score, their_score):
+    bs, cs = 0
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(their_history) >= 1: # Make sure they have a history
+        for i in their_history: # Root throught their history
+            if i == 'b':
+                bs = bs + 1;
+            else:
+                cs = cs + 1;
+        tot = bs+cs;
+        if cs/tot > bs/tot:
+            return 'c';
+        else:
+            return 'b';
+    else:                     # If they don't have a history...
+        coin =  randint(1,2); # Flip a 'coin'
+        if coin == 1:
+            return 'b';
+        else:
+            return 'c';
+     
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,9 +43,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
