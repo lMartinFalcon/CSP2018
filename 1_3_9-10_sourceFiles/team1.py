183d414fe4f00ea60a205423200cb5d77d46c7b8
Clayton Jones <743874@nkcschools.org>
2018-12-14 13:44:49 -0600

    TeamOne

--- 1_3_9-10_sourceFiles/team1.py
+++ 1_3_9-10_sourceFiles/team1.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Not Losers' # Only 10 chars displayed.
+strategy_name = 'Risk it for the biscuit'
+strategy_description = 'Uses probability to predict opponets answers and counters them.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,23 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    bcount = 0
+    ccount = 0
+     
+    if their_history[-1] == 'b':
+        bcount += 1
+    elif their_history[-1] == 'c':
+        ccount += 1    
+    
+    if bcount > ccount:
+        return 'c'
+    elif ccount > bcount:
+        return 'b'
+    else:
+        return 'b'        
+     
+      
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +42,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
